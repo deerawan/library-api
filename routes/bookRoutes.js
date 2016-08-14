b0e9a598c42bed41d02a8502527eb490292705f8
Budi Irawan <deerawan@gmail.com>
2016-08-14 22:36:23 +0700

    add DELETE

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -20,23 +20,48 @@ var routes = function(Book) {
       });
     });
 
+  bookRouter.use('/:id', (req, res, next) => {
+    Book.findById(req.params.id, (err, book) => {
+      if (err) {
+        res.status(500).send(err);
+      } else if (book) {
+        req.book = book;
+        next();
+      } else {
+        res.status(404).send('no book found');
+      }
+    });
+  });
   bookRouter.route('/:id')
     .get((req, res) => {
-      Book.findById(req.params.id, (err, book) => {
-        if (err) { res.status(500).send(err); }
-        res.json(book);
-      });
+      res.json(req.book);
     })
     .put((req, res) => {
-      Book.findById(req.params.id, (err, book) => {
+      req.book.title = req.body.title;
+      req.book.author = req.body.author;
+      req.book.genre = req.body.genre;
+      req.book.read = req.body.read;
+      req.book.save(function(err) {
         if (err) { res.status(500).send(err); }
-        console.log(req.body);
-        book.title = req.body.title;
-        book.author = req.body.author;
-        book.genre = req.body.genre;
-        book.read = req.body.read;
-        book.save();
-        res.json(book);
+        res.json(req.book);
+      });
+    })
+    .patch((req, res) => {
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      for (var field in req.body) {
+        req.book[field] = req.body[field];
+      }
+      req.book.save(function(err) {
+        if (err) { res.status(500).send(err); }
+        res.json(req.book);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) { res.status(500).send(err); }
+        res.status(204).send('book is deleted');
       });
     });
 
