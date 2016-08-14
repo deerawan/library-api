0172fdc7a5ae1591db31ea8dd8aaaa2bdd1f13a3
Budi Irawan <deerawan@gmail.com>
2016-08-14 20:08:26 +0700

    GET books

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/libraryAPI');
+var Book = require('./models/bookModel');
+
 
 var app = express();
 var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    var query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
+      if (err) { res.status(500).send(err); }
+      res.json(books);
+    });
+  });
+bookRouter.route('/books/:id')
+  .get((req, res) => {
+    Book.findById(req.params.id, (err, books) => {
+      if (err) { res.status(500).send(err); }
+      res.json(books);
+    });
+  });
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to my API');
